5f0a185acc1ceee10643e663b1696c14a7682d75
Harshendra Avabratha <harshendraa@gmail.com>
2016-07-20 10:50:04 +0530

    Display and infobox index completed for animals, prog. languages and planets

--- getdiff.py
+++ getdiff.py
@@ -1,4 +1,6 @@
 
+import os
+
 properties = {}
 properties['country'] = {
     'capital',
@@ -18,10 +20,38 @@ properties['country'] = {
     'flower'
 }
 
-print 'Enter two entities'
+properties['animal'] = {
+    'phylum',
+    'subphylum',
+    'classis',
+    'familia',
+    'genus',
+    'species'
+}
+
+properties['programming_languages'] = {
+    'paradigm',
+    'designer',
+    'developer',
+    'writtenIn',
+    'influencedBy',
+    'influenced',
+    'latestReleaseVersion',
+    'operatingSystem',
+    'license'
+}
+
+print 'Categories present:'
+for cat in properties.keys():
+    print cat
+
+print 
+print 'Entities list:'
+os.system('ls index')
+print 
+print 'Enter category entity'
 inp = raw_input()
-A,B = inp.split(' ')
-category = 'country'
+category,A = inp.split(' ')
 path = './index/'
 
 
@@ -30,6 +60,20 @@ with open(path+A) as f:
     lines = f.readlines()
 
 
+diff_list = {}
+for line in lines:
+    line = line.strip()
+
+    property = line.split(' ')[0]
+    value = ' '.join(line.split(' ')[1:])
+
+    if property in properties[category]:
+            diff_list[property] = value
+
+for property,value in diff_list.items(): 
+    print "%-25s %40s" % (property,value)
+
+'''
 diff_list = {}
 for line in lines:
     line = line.strip()
@@ -52,7 +96,7 @@ for line in lines:
 
 for property,values in diff_list.items(): 
     print "%-25s %40s %40s" % (property,values[0],values[1])
-
+'''
 
 
 
