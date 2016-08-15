4f3e483f47a3ea9ffe49527c841f021b9bd5e5bc
Md Tareque Khan <mohdtarequekhan@gmail.com>
2016-08-16 01:26:51 +0530

    output added

--- scripts/hypernym_hierarchy.py
+++ scripts/hypernym_hierarchy.py
@@ -3,6 +3,37 @@ from nltk.corpus import wordnet
 import sys
 
 
+"""
+This program outputs all the hypernyms till the most higher level hypernyms is found
+
+Sample Output
+ python hypernym_hierarchy.py lion
+ hypernyms of lion = big_cat.n.01
+ hypernyms of big_cat = feline.n.01
+ hypernyms of feline = carnivore.n.01
+ hypernyms of carnivore = placental.n.01
+ hypernyms of placental = mammal.n.01
+ hypernyms of mammal = vertebrate.n.01
+ hypernyms of vertebrate = chordate.n.01
+ hypernyms of chordate = animal.n.01
+ hypernyms of animal = organism.n.01
+ hypernyms of organism = living_thing.n.01
+ hypernyms of living_thing = whole.n.02
+ hypernyms of whole = concept.n.01
+ hypernyms of concept = idea.n.01
+ hypernyms of idea = content.n.05
+ hypernyms of content = collection.n.01
+ hypernyms of collection = group.n.01
+ hypernyms of group = abstraction.n.06
+ hypernyms of abstraction = concept.n.01
+ Cycle detected [concept]
+
+ lion --> big_cat --> feline --> carnivore --> placental --> mammal --> vertebrate --> chordate --> animal --> organism --> living_thing --> whole --> concept --> idea --> content --> collection --> group --> abstraction --> concept
+
+
+"""
+
+
 def usage():
     print """usage: python hypernym_hierarchy.py <obj>
 e.g.
